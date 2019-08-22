8f2c9f7454e77326105cd2d7b52bf9b95bf60426
Alexander <redlesleys@gmail.com>
2019-08-22 23:27:50 +0300

    Develop
    + some fixes;

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,11 +3,11 @@
     <h2 class="center teal-text">Chatter</h2>
     <div class="card">
       <div class="card-content">
-        <ul class="messages">
-          <li>
-            <span class="teal-text">Name</span>
-            <span class="grey-text text-darken-3">message</span>
-            <span class="grey-text time">time</span>
+        <ul class="messages" v-chat-scroll>
+          <li v-for="message in messages" :key="message.id">
+            <span class="teal-text">{{ message.name }}</span>
+            <span class="grey-text text-darken-3">{{ message.content }}</span>
+            <span class="grey-text time">{{ message.timestamp }}</span>
           </li>
         </ul>
       </div>
@@ -20,6 +20,8 @@
 
 <script>
   import NewMessage from '@/components/NewMessage'
+  import db from '@/firebase/init'
+  import moment from 'moment'
 
   export default {
     name: "Chat",
@@ -29,8 +31,25 @@
     },
     data() {
       return {
-
+        messages: []
       }
+    },
+    created() {
+      let ref = db.collection('messages').orderBy('timestamp')
+
+      ref.onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type == 'added') {
+            let doc = change.doc
+            this.messages.push({
+              id: doc.id,
+              name: doc.data().name,
+              content: doc.data().content,
+              timestamp: moment(doc.data().timestamp).format('lll')
+            })
+          }
+        })
+      })
     }
   }
 </script>
@@ -45,6 +64,19 @@
   }
   .chat .time {
     display: block;
-    font-size: 1.2em;
+    font-size: 0.8em;
+  }
+  .messages {
+    max-height: 300px;
+    overflow: auto;
+  }
+  .messages::-webkit-scrollbar {
+    width: 3px;
+  }
+  .messages::-webkit-scrollbar-track {
+    background: #ddd;
+  }
+  .messages::-webkit-scrollbar-thumb {
+    background: #aaa;
   }
 </style>
