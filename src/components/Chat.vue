b8d69a3f3d9377f6ec569f7f48fe85970b40b77c
Alexander <redlesleys@gmail.com>
2019-08-22 22:58:14 +0300

    Develop
    + added main components;

---
+++ src/components/Chat.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="chat container">
+    <h2 class="center teal-text">Chatter</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <NewMessage :name="name" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import NewMessage from '@/components/NewMessage'
+
+  export default {
+    name: "Chat",
+    props: ['name'],
+    components: {
+      NewMessage
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+  .chat h2 {
+    font-size: 2.6em;
+    margin-bottom: 40px;
+  }
+  .chat span {
+    font-size: 1.4em;
+  }
+  .chat .time {
+    display: block;
+    font-size: 1.2em;
+  }
+</style>
