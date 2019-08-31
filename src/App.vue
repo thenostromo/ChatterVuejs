b2aa6f03b42240488cde67b167b427a9387b0850
Devandimp <parsing.net@gmail.com>
2019-08-31 23:17:01 +0300

    DEVELOP

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,32 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import db from '@/firebase/init'
+  import moment from 'moment'
+
+  export default {
+    name: 'App',
+    created() {
+      let ref = db.collection('messages').orderBy('timestamp')
+
+      ref.onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type == 'added') {
+            let doc = change.doc
+            this.$store.commit('addMessage', {
+              id: doc.id,
+              name: doc.data().name,
+              content: doc.data().content,
+              timestamp: moment(doc.data().timestamp).format('lll')
+            })
+          }
+        })
+      })
+    }
+  }
 </script>
 
 <style>
 
 </style>
+<style src='./assets/css/main.css'></style>
--- src/main.js
+++ src/main.js
@@ -2,17 +2,41 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex';
 import router from './router'
 import VueChatScroll from "vue-chat-scroll"
 
+Vue.use(Vuex);
 Vue.use(VueChatScroll)
 
 Vue.config.productionTip = false
+
+const VueStorage = new Vuex.Store({
+  strict: true,
+  state: {
+    messages: []
+  },
+  mutations: {
+    addMessage(state, value) {
+      state.messages.push(value)
+    },
+    setMessages(state, value) {
+      state.messages = value
+    }
+  },
+  getters: {
+    getMessages(state) {
+      return state.messages;
+    }
+  }
+})
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store: VueStorage,
   components: { App },
   template: '<App/>'
 })
